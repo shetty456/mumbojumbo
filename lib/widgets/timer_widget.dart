4ed4dd95b436dbd86535b6955715961c1fc23adc
Sunil Hanamshetty <64653515+shetty456@users.noreply.github.com>
2024-10-13 20:57:26 +0530

    add assets

--- lib/widgets/timer_widget.dart
+++ lib/widgets/timer_widget.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart'; // Import audioplayers package
 
 class TimerWidget extends StatefulWidget {
   final VoidCallback onTimeout;
@@ -13,11 +14,30 @@ class TimerWidget extends StatefulWidget {
 class _TimerWidgetState extends State<TimerWidget> {
   int _remainingTime = 30;
   late Timer _timer;
+  final AudioPlayer _audioPlayer = AudioPlayer(); // Initialize AudioPlayer
 
   @override
   void initState() {
     super.initState();
     _startTimer();
+    _playBackgroundMusic(); // Start the background music when the timer starts
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    _stopBackgroundMusic(); // Stop music when widget is disposed
+    super.dispose();
+  }
+
+  // Function to start the background music
+  void _playBackgroundMusic() async {
+    await _audioPlayer.play(AssetSource('sounds/bg_music.mp3'), volume: 0.5); // Playing the asset
+  }
+
+  // Function to stop the background music
+  void _stopBackgroundMusic() async {
+    await _audioPlayer.stop(); // Stop the music
   }
 
   void _startTimer() {
@@ -26,17 +46,12 @@ class _TimerWidgetState extends State<TimerWidget> {
         if (_remainingTime > 0) {
           _remainingTime--;
         } else {
-          widget.onTimeout();
+          widget.onTimeout(); // Notify parent when time runs out
           _timer.cancel();
+          _stopBackgroundMusic(); // Stop the music when time runs out
         }
       });
     });
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
   }
 
   @override
