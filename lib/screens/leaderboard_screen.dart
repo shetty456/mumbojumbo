33beb0917bfd3052d054bfd5a33fabab030a4fc4
Shrabanti <shrabantimazumdar72@gmail.com>
2024-10-14 21:10:15 +0530

    adding jumbleword model and other changes

--- lib/screens/leaderboard_screen.dart
+++ lib/screens/leaderboard_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class LeaderboardScreen extends StatelessWidget {
+  const LeaderboardScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     // Dummy leaderboard data for now
--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -3,21 +3,23 @@ import 'package:go_router/go_router.dart';
 import 'package:mumbojumbo/common/router.dart';
 
 class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Welcome to Mumbo Jumbo')),
+      appBar: AppBar(title: const Text('Welcome to Mumbo Jumbo')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Welcome to the Game!', style: TextStyle(fontSize: 24)),
-            SizedBox(height: 20),
-            Text('Rules: Solve anagrams in 30 seconds!'),
-            SizedBox(height: 20),
+            const Text('Welcome to the Game!', style: TextStyle(fontSize: 24)),
+            const SizedBox(height: 20),
+            const Text('Rules: Solve anagrams in 30 seconds!'),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () => context.go(AppRoutePaths.gamezone),
-              child: Text('Start Game'),
+              child: const Text('Start Game'),
             ),
           ],
         ),
